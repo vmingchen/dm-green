257df136043dda699b9c7fef52d4b0792a49f309
brianchenming <brianchenming@gmail.com>
2012-02-20 03:59:06 -0500

    add header

--- dm-energy.c
+++ dm-energy.c
@@ -12,43 +12,119 @@
 #include <linux/blkdev.h>
 #include <linux/bio.h>
 #include <linux/log2.h>
+#include <linux/dm-io.h>
 
 #include <linux/device-mapper.h>
 
 #define DM_MSG_PREFIX "energy"
 
+/*
+ * Magic for persistent energy header: "EnEg"
+ */
+#define ENERGE_MAGIC 0x45614567
+#define ENERGE_VERSION 1
+
 #define array_too_big(fixed, obj, num) \
 	((num) > (UINT_MAX - (fixed)) / (obj))
 
-struct mapped_disk {
-    struct dm_dev *dev;
-    sector_t start;
-    uint64_t ext_count;     /* number of extents */
-    
-    atomic_t err_count;     /* number of errors */
-};
+/*
+ * Header on disk, followed by metadata for mapped_disk and energy_map_entry.
+ */
+struct energy_header_disk {
+    __le32 magic;
+    __le32 version;
+    __le32 ndisk;
+    __le32 ext_size;
+    __le64 ext_count;
+} __packed;
 
-struct energy_c {
+/*
+ * Header in memory, contained in energy context (energy_c).
+ */
+struct energy_header_core {
+    uint32_t magic;
+    uint32_t version;
     uint32_t ndisk;
     uint32_t ext_size;
     uint64_t ext_count;
-
-    uint32_t ext_shift;
-
-    struct mapped_disk disks[0];
 };
 
-static struct energy_c *alloc_context(unsigned int ndisk)
+struct energy_map_entry {
+    uint64_t mapped_id;
+    uint32_t flags;
+    uint32_t freq;              /* how many times are accessed */
+};
+
+struct energy_extent {
+    struct energy_map_entry entry;
+    uint64_t tick;              /* timestamp of latest access */
+    atomic_t ref_count;
+};
+
+struct mapped_disk {
+    struct dm_dev *dev;
+    uint64_t ext_count;         /* number of extents */
+    atomic_t err_count;         /* number of errors */
+};
+
+struct energy_c {
+    struct dm_target *ti;
+
+    struct energy_header_core header;
+    uint32_t flags;
+    uint32_t ext_shift;
+
+    struct mapped_disk *disks;
+    struct energy_extent *table;
+
+    struct dm_io_request io_req;
+
+    uint64_t migration_ext;     /* logical extent id under migration */
+    uint64_t migration_src;     /* source physical extent id */
+    uint64_t migration_dst;     /* dest physical extent id */
+};
+
+static struct energy_c *alloc_context(uint32_t ndisk)
 {
-    size_t len;
+    struct energy_c *ec;
 
-    if (array_too_big(sizeof(struct energy_c), 
-                sizeof(struct mapped_disk), ndisk))
+    ec = kmalloc(sizeof(struct energy_c), GFP_KERNEL);
+    if (!ec)
+        return ec;
+
+    ec->disks = kmalloc(sizeof(struct mapped_disk) * ndisk, GFP_KERNEL);
+    if (!ec->disks) {
+        kfree(ec);
         return NULL;
+    }
+    
+    ec->table = NULL;       /* table not allocated yet */
 
-    len = sizeof(struct energy_c) + (sizeof(struct mapped_disk) * ndisk);
+    return ec;
+}
 
-    return kmalloc(len, GFP_KERNEL);
+static void free_context(struct energy_c *ec)
+{
+    BUG_ON(!ec || !(ec->disks));
+
+    if (ec->table) {
+        kfree(ec->table);
+    }
+
+    kfree(ec->disks);
+    kfree(ec);
+}
+
+static void header_to_disk(struct energy_header_core *core, 
+        struct energy_header_disk *disk)
+{   /* TODO */
+    return NULL;
+}
+
+static void header_from_disk(struct energy_header_core *core,
+        struct energy_header_disk *disk)
+{   /* TODO */
+    return NULL;
 }
 
 /*
@@ -79,12 +155,12 @@ static int get_mdisk(struct dm_target *ti, struct energy_c *ec,
 
 /*
  * Construct an energy mapping.
- *  <extent size> <number of disks> [<dev> <offset> <length>]+
+ *  <extent size> <number of disks> [<dev> <number-of-extent>]+
  */
 static int energy_ctr(struct dm_target *ti, unsigned int argc, char **argv)
 {
     uint32_t ndisk;
-    uint32_t extent_size;
+    uint32_t ext_size;
 	char *end;
     struct energy_c *ec;
     unsigned i;
@@ -92,19 +168,19 @@ static int energy_ctr(struct dm_target *ti, unsigned int argc, char **argv)
 
     DMERR("energy_ctr (argc: %d)\n", argc);
 
-    if (argc < 5) {
+    if (argc < 4) {
         ti->error = "Not enough arguments";
         return -EINVAL;
     }
 
-	extent_size = simple_strtoul(argv[0], &end, 10);
-	if (*end || !is_power_of_2(extent_size) 
-            || (extent_size < (PAGE_SIZE >> SECTOR_SHIFT))) {
+	ext_size = simple_strtoul(argv[0], &end, 10);
+	if (*end || !is_power_of_2(ext_size) 
+            || (ext_size < (PAGE_SIZE >> SECTOR_SHIFT))) {
 		ti->error = "Invalid extent size";
 		return -EINVAL;
 	}
 
-    if (ti->len & (extent_size -1)) {
+    if (ti->len & (ext_size -1)) {
         ti->error = "Target length not divisible by extent size";
         return -EINVAL;
     }
@@ -115,8 +191,8 @@ static int energy_ctr(struct dm_target *ti, unsigned int argc, char **argv)
         return -EINVAL;
     }
 
-    if (argc != (2 + 3*ndisk)) {
-        ti->error = "Disk parameters not match disk count";
+    if (argc != (2 + 2*ndisk)) {
+        ti->error = "Number of parameters mismatch";
         return -EINVAL;
     }
 
@@ -127,9 +203,9 @@ static int energy_ctr(struct dm_target *ti, unsigned int argc, char **argv)
     }
 
     ec->ndisk = ndisk;
-    ec->ext_size = extent_size;
+    ec->ext_size = ext_size;
     ec->ext_count = 0;
-    ec->ext_shift = ffs(extent_size) - 1;
+    ec->ext_shift = ffs(ext_size) - 1;
 
     for (i = 0, argv += 2; i < ndisk; ++i, argv += 2) {
         r = get_mdisk(ti, ec, i, argv);
